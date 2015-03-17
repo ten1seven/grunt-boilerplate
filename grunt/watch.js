dea3d8339b0db84ca3bc5ab0451c04729c135602
Jeremy Fields <jeremy.fields@viget.com>
2015-03-17 17:01:29 -0600

    Update watch.js

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,8 +1,28 @@
 module.exports = {
   configFiles: {
-    files: ['Gruntfile.js'],
+    files: [
+      'Gruntfile.js'
+    ],
     options: {
       reload: true
+    }
+  },
+  images: {
+    files: [
+      './src/images/**/*.{png,jpg,jpeg,gif}'
+    ],
+    tasks: ['images'],
+    options: {
+      spawn: false
+    }
+  },
+  markup: {
+    files: [
+      './pages/**/*.html'
+    ],
+    tasks: ['html'],
+    options: {
+      spawn: false
     }
   },
   scripts: {
@@ -15,15 +35,10 @@ module.exports = {
     }
   },
   styles: {
-    files: './src/styles/**/*.scss',
+    files: [
+      './src/styles/**/*.scss'
+    ],
     tasks: ['styles'],
-    options: {
-      spawn: false
-    }
-  },
-  images: {
-    files: './src/images/**/*.{png,jpg,jpeg,gif}',
-    tasks: ['images'],
     options: {
       spawn: false
     }
