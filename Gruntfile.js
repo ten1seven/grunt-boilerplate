b05b0c6efd58f3dc68cbe4a6962b952f1b630d4d
Jeremy Fields <jeremy.fields@viget.com>
2015-03-23 14:27:50 -0600

    Adding jitGrunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,6 @@ module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
   // load grunt config
-  require('load-grunt-config')(grunt);
+  require('load-grunt-config')(grunt, { jitGrunt: true });
 
 };
